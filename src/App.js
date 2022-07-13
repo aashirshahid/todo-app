37c0ac8fceab51db6f41acef6856fd8ee716c2db
Aashir Shahid <aashahid@squaretrade.com>
2022-07-13 13:49:40 +0500

    Intiail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function TodoList() {
+	const [inputValue, setInputValue] = useState("");
+	const [todoList, setTodoList] = useState([]);
+	const addBtnRef = useRef();
+
+	useEffect(() => {
+		const Arr = JSON.parse(localStorage.getItem("todoList")) || [];
+		setTodoList(Arr);
+	}, []);
+
+	const fieldChangeHandler = (e) => {
+		if (e.keyCode && e.keyCode === 13) {
+			addBtnRef.current.click();
+			return;
+		}
+		setInputValue(e.target.value);
+	};
+
+	const setLocalStorage = (value) => {
+		localStorage.setItem("todoList", JSON.stringify(value));
+	};
+
+	const addHandler = () => {
+		setTodoList((previous) => {
+			setLocalStorage([...previous, inputValue]);
+			return [...previous, inputValue];
+		});
+		setInputValue("");
+	};
+
+	const deleteItem = (index) => {
+		const updatedTodoList = [...todoList];
+		updatedTodoList.splice(index, 1);
+		setLocalStorage(updatedTodoList);
+		setTodoList(updatedTodoList);
+	};
+
+	const todoListHandler = (e) => {
+		if (e.target.classList.contains("icon")) {
+			deleteItem(e.target.id);
+		}
+	};
+
+	const deleteAllITem = () => {
+		setInputValue([]);
+		setLocalStorage([]);
+	};
+
+	return (
+		<div className="wrapper">
+			<header>Todo App</header>
+			<div className="inputField">
+				<input
+					type="text"
+					placeholder="Add your new todo item..."
+					value={inputValue}
+					onChange={fieldChangeHandler}
+					onKeyDown={fieldChangeHandler}
+				/>
+				<button onClick={addHandler} ref={addBtnRef}>
+					Add
+				</button>
+			</div>
+			<ul className="todoList" onClick={todoListHandler}>
+				{React.Children.toArray(
+					todoList.map((todo, index) => (
+						<li className="bordered">
+							{todo}
+							<span className="icon" id={index}>
+								X
+							</span>
+						</li>
+					))
+				)}
+			</ul>
+			<div className="footer">
+				<span>
+					You have{" "}
+					<span className="pendingTask">{todoList.length}</span>{" "}
+					pending tasks
+				</span>
+				<button onClick={deleteAllITem}>Clear All</button>
+			</div>
+		</div>
+	);
 }
 
-export default App;
+export default TodoList;
